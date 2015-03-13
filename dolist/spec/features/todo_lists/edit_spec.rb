f7bd4da4561c81d7368e0c8bf6cd8e002c5d7c62
Van Phan <VanPhan514@gmail.com>
2015-03-13 15:33:05 -0400

    Added destroy spec and edited controller

--- dolist/spec/features/todo_lists/edit_spec.rb
+++ dolist/spec/features/todo_lists/edit_spec.rb
@@ -1,16 +1,30 @@
 require 'spec_helper'
 
 describe "Editing todo lists" do
-	it "updates a todo list successfully with correct information" do
-	todo_list = TodoList.create(title: "Groceries", description: "Grocery list.")
-		
+	let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.")}
 
-	visit "/todo_lists"
+	def update_todo_list(option={})
+		options[:title] ||= "My todo list"
+		options[:description] ||= "This is my todo list"
 
-	fill_in "Title", with: "New title"
-	fill_in "Description", with: "New description"
-	click_button "Update Todo list"
+		todo_list = options[:todo_list]
 
+		visit "/todo_lists"
+		within "#todo_list_#{todo_list.id}" do
+			click_link "Edit"
+		end
+
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
+		click_button "Update Todo list"
+	end
+
+	it "updates a todo lists successfully with correct information" do
+	
+	update_todo_list todo_list: todo_list, 
+					 title: "New title", 
+					 description: "New description"
+	
 	todo_list.reload
 
 	expect(page).to have_content("Todo list was successfully updated")
